14baf1a0adb62588538e1e003ecfd8dd70006bad
OWenT <owt5008137@live.com>
2014-03-11 19:16:54 +0800

    fix gtest conf of vc
    1. fix gtest conf of vc, there is no pthread in vc
    2. fix source group

--- src/libcopp.macro.cmake
+++ src/libcopp.macro.cmake
@@ -5,5 +5,3 @@ set (PROJECT_LIBCOPP_LIB_LINK copp)
 # ========== filter sources files ==========
 file(GLOB_RECURSE COPP_SRC_LIST ${PROJECT_LIBCOPP_INC_DIR}/*.h ${PROJECT_LIBCOPP_INC_DIR}/*.hpp)
 include_macro_recurse(FILTER "*.src.filter.cmake" "${PROJECT_LIBCOPP_SRC_DIR}")
-source_group_by_dir(COPP_SRC_LIST)
-
