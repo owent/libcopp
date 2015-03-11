0da007a4a89e53705687a2a3892efd058b059e87
OWenT <owt5008137@live.com>
2015-03-11 17:59:46 +0800

    Update task.h

--- include/libcotask/task.h
+++ include/libcotask/task.h
@@ -42,7 +42,7 @@ namespace cotask {
         typedef impl::task_impl::action_ptr_t action_ptr_t;
         typedef impl::task_impl::ptr_t task_ptr_t;
 
-        friend task_allocator_t;
+        friend class task_allocator_t;
     private:
         task() {
             id_allocator_t id_alloc_;
