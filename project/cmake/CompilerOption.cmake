aceaa5ae20d9fbd07284b8538afbc5152b8dcdcf
owent <owt5008137@live.com>
2017-09-30 20:21:49 +0800

    当前版本的clang和libstdc++（gcc 7.2.0）搭配会编译不通过，闲关掉clang编译

--- project/cmake/CompilerOption.cmake
+++ project/cmake/CompilerOption.cmake
@@ -8,8 +8,8 @@ else()
 endif()
 
 if(NOT CMAKE_BUILD_TYPE)
-	set(CMAKE_BUILD_TYPE "Debug")
-	#set(CMAKE_BUILD_TYPE "RelWithDebInfo")
+	#set(CMAKE_BUILD_TYPE "Debug")
+	set(CMAKE_BUILD_TYPE "RelWithDebInfo")
 endif()
 
 # 编译器选项 (仅做了GCC、VC和Clang兼容)
