856c15a33bafa29b673d522cdb33a816119b40d8
owent <admin@owent.net>
2017-12-19 17:35:07 +0800

    add sys name in cmake_dev.bat

--- cmake_dev.bat
+++ cmake_dev.bat
@@ -1,4 +1,4 @@
-mkdir build
-cd build
+mkdir build_windows_msvc
+cd build_windows_msvc
 
 cmake .. -G "Visual Studio 15 2017 Win64" -DLIBCOPP_FCONTEXT_USE_TSX=ON -DPROJECT_ENABLE_UNITTEST=ON -DPROJECT_ENABLE_SAMPLE=ON -DCMAKE_BUILD_TYPE=Debug
