606f9c43c4d31a67f9414ea5b828b513e10404c2
owentou <owentou@tencent.com>
2019-12-12 09:06:25 +0800

    sync compile_optimize.h from atframe_utils

--- include/libcopp/utils/config/compile_optimize.h
+++ include/libcopp/utils/config/compile_optimize.h
@@ -43,57 +43,38 @@
 // ---------------- branch prediction information ----------------
 
 // ================ import/export ================
+// @see https://gcc.gnu.org/wiki/Visibility
+// @see http://releases.llvm.org/9.0.0/tools/clang/docs/AttributeReference.html
 // 不支持 borland/sunpro_cc/xlcpp
 
 // ================ import/export: for compilers ================
-#if defined(__INTEL_COMPILER) || defined(__ICL) || defined(__ICC) || defined(__ECC)
-//  Intel
-//
-// Dynamic shared object (DSO) and dynamic-link library (DLL) support
-//
-#if defined(__GNUC__) && (__GNUC__ >= 4)
-#define UTIL_SYMBOL_EXPORT __attribute__((visibility("default")))
-#define UTIL_SYMBOL_IMPORT
-#define UTIL_SYMBOL_VISIBLE __attribute__((visibility("default")))
-#endif
-
-#elif defined __clang__ && !defined(__CUDACC__) && !defined(__ibmxl__)
-// when using clang and cuda at same time, you want to appear as gcc
-//  Clang C++ emulates GCC, so it has to appear early.
-//
-
-// Dynamic shared object (DSO) and dynamic-link library (DLL) support
-//
-#if !defined(_WIN32) && !defined(__WIN32__) && !defined(WIN32)
-#define UTIL_SYMBOL_EXPORT __attribute__((__visibility__("default")))
-#define UTIL_SYMBOL_IMPORT
-#define UTIL_SYMBOL_VISIBLE __attribute__((__visibility__("default")))
-#endif
-
-#elif defined(__GNUC__) && !defined(__ibmxl__)
-//  GNU C++:
+#if defined(__GNUC__) && !defined(__ibmxl__)
+//  GNU C++/Clang
 //
 // Dynamic shared object (DSO) and dynamic-link library (DLL) support
 //
 #if __GNUC__ >= 4
-#if (defined(_WIN32) || defined(__WIN32__) || defined(WIN32)) && !defined(__CYGWIN__)
+#if defined(_WIN32) || defined(__WIN32__) || defined(WIN32) || defined(__CYGWIN__)
 // All Win32 development environments, including 64-bit Windows and MinGW, define
 // _WIN32 or one of its variant spellings. Note that Cygwin is a POSIX environment,
 // so does not define _WIN32 or its variants.
-#define BOOST_HAS_DECLSPEC
 #define UTIL_SYMBOL_EXPORT __attribute__((__dllexport__))
 #define UTIL_SYMBOL_IMPORT __attribute__((__dllimport__))
 #else
 #define UTIL_SYMBOL_EXPORT __attribute__((__visibility__("default")))
-#define UTIL_SYMBOL_IMPORT
-#endif
+#define UTIL_SYMBOL_IMPORT __attribute__((__visibility__("default")))
 #define UTIL_SYMBOL_VISIBLE __attribute__((__visibility__("default")))
+#define UTIL_SYMBOL_HIDDEN __attribute__((__visibility__("hidden")))
+#endif
 #else
 // config/platform/win32.hpp will define UTIL_SYMBOL_EXPORT, etc., unless already defined
 #define UTIL_SYMBOL_EXPORT
+#define UTIL_SYMBOL_IMPORT
+#define UTIL_SYMBOL_VISIBLE
+#define UTIL_SYMBOL_HIDDEN
 #endif
 
-#elif defined _MSC_VER
+#elif defined(_MSC_VER)
 //  Microsoft Visual C++
 //
 //  Must remain the last #elif since some other vendors (Metrowerks, for
@@ -107,7 +88,7 @@
 //  If a compiler doesn't support __declspec(dllexport)/__declspec(dllimport),
 //  its boost/config/compiler/ file must define UTIL_SYMBOL_EXPORT and
 //  UTIL_SYMBOL_IMPORT
-#ifndef UTIL_SYMBOL_EXPORT
+#if !defined(UTIL_SYMBOL_EXPORT) && (defined(_WIN32) || defined(__WIN32__) || defined(WIN32) || defined(__CYGWIN__))
 #define UTIL_SYMBOL_EXPORT __declspec(dllexport)
 #define UTIL_SYMBOL_IMPORT __declspec(dllimport)
 #endif
@@ -122,11 +103,14 @@
 #ifndef UTIL_SYMBOL_VISIBLE
 #define UTIL_SYMBOL_VISIBLE
 #endif
+#ifndef UTIL_SYMBOL_HIDDEN
+#define UTIL_SYMBOL_HIDDEN
+#endif
 
 // ---------------- import/export ----------------
 
 // ================ __cdecl ================
-#if defined(__GNUC__)
+#if defined(__GNUC__) || defined(__GNUG__)
 #ifndef __cdecl
 // see https://gcc.gnu.org/onlinedocs/gcc-4.0.0/gcc/Function-Attributes.html
 // Intel x86 architecture specific calling conventions
@@ -138,5 +122,41 @@
 #endif
 #endif
 // ---------------- __cdecl ----------------
+
+// ================ always inline ================
+
+#ifndef UTIL_FORCEINLINE
+#if defined(__clang__)
+#if __has_attribute(always_inline)
+#define UTIL_FORCEINLINE __attribute__((always_inline))
+#else
+#define UTIL_FORCEINLINE inline
+#endif
+
+#elif defined(__GNUC__) && __GNUC__ > 3
+#define UTIL_FORCEINLINE __attribute__((always_inline))
+#elif defined(_MSC_VER)
+#define UTIL_FORCEINLINE __forceinline
+#else
+#define UTIL_FORCEINLINE inline
+#endif
+#endif
+
+#ifndef UTIL_NOINLINE
+#if defined(_MSC_VER)
+#define UTIL_NOINLINE __declspec(noinline)
+#elif defined(__GNUC__) && __GNUC__ > 3
+// Clang also defines __GNUC__ (as 4)
+#if defined(__CUDACC__)
+// nvcc doesn't always parse __noinline__,
+// see: https://svn.boost.org/trac/boost/ticket/9392
+#define UTIL_NOINLINE __attribute__((noinline))
+#else
+#define UTIL_NOINLINE __attribute__((__noinline__))
+#endif
+#else
+#define UTIL_NOINLINE
+#endif
+#endif
 
 #endif
