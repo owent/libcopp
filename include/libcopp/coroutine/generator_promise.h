2d45a6dcae67176225857254d1a8deec9f221f03
owent <admin@owent.net>
2025-03-12 17:28:45 +0800

    Fixes compiling

--- include/libcopp/coroutine/generator_promise.h
+++ include/libcopp/coroutine/generator_promise.h
@@ -416,7 +416,7 @@ class LIBCOPP_COPP_API_HEAD_ONLY generator_vtable_delegate<TCONTEXT, generator_v
 
   template <class TSUSPEND>
   LIBCOPP_UTIL_FORCEINLINE generator_vtable_delegate(TSUSPEND&& await_suspend_callback) noexcept
-      : vtable_(new vtable_type(std::forward<TSUSPEND>(await_suspend_callback)), nullptr) {}
+      : vtable_(new vtable_type(std::forward<TSUSPEND>(await_suspend_callback), nullptr)) {}
 
   LIBCOPP_UTIL_FORCEINLINE generator_vtable_delegate() noexcept : vtable_(nullptr) {}
 
